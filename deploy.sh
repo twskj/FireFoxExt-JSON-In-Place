02b3e263bec687723043e90d9a9f895fb6ff93cf
twskj <Tawesak.Jongsuksawas@chrobinson.com>
2018-12-25 16:25:48 -0600

    add: auto package script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -rf ../jipy
+mkdir ../jipy
+cp -r background.js main.js manifest.json icon ../jipy/
+rm ../firefox-jipy.zip
+cd ../jipy
+zip -r firefox-jipy.zip * 
+mv firefox-jipy.zip ../firefox-jipy.zip
